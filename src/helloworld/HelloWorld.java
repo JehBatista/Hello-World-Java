88c80329969b3602395e15333e442178a0f2a516
JehBatista <jessicabatista609@gmail.com>
2017-03-16 16:13:49 -0300

    modifiquei o codigo para que quando eu digitasse o nome ele imprimisse junto com o ola

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -19,6 +19,11 @@ package helloworld;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
+		if(args.length > 0){
+		System.out.println("Ola, " + args[0]);
+		}else {
 		System.out.println("Ola, Mundo");
+		}
 	}
 }
+
